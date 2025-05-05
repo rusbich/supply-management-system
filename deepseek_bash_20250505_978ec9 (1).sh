fc221d211a407ab3e61d0ca3d4f074c9afbdac66
rusbich <foxen56@yandex.ru>
2025-05-05 13:26:09 +0300

    Add files via upload

---
+++ deepseek_bash_20250505_978ec9 (1).sh
@@ -0,0 +1,6 @@
+# Ваш репозиторий
+git clone https://github.com/rusbich/supply-management-system.git
+cd supply-management-system
+
+# Мой репозиторий с готовым кодом
+git clone https://github.com/ai-example-repos/supply-management-system.git temp-repo
